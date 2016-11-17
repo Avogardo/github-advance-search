e48bb720406aa40e9656d9b6c53c11217f3eb654
Avogardo <avogardo0@gmail.com>
2016-11-17 21:59:36 +0100

    0.5
    Fetch i state działa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Requests from './Requests.js';
+
+
+class App extends Component {
+  render() {
+    return(
+
+        <Requests />
+    )
+  }
+}
+
+export default App;
---
+++ src/Requests.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+//import fetch from 'fetch';
+
+class Requests extends Component {
+
+  constructor (props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      results: []
+    };
+  }
+
+  onSubmit (e) {
+    e.preventDefault();
+    const issue = e.target.value;
+
+    fetch("https://api.github.com/search/issues?q="+issue).then(function(response) {
+        return response.json();
+    }).then((data) => {
+
+        let result = "";
+        for(let i = 0; i < 30; i++) {
+            result += data.items[i].html_url + "<br>";
+        }
+
+        this.setState({
+          results: result
+        });
+
+    }).catch(function() {
+      console.log("error");
+    });
+  }
+
+  render() {
+
+    return (
+            <div>
+                <h2>Szukaj</h2>
+                <input type="text" onChange={this.onSubmit} />
+
+                <h2>Wyniki:</h2>
+                <p>{this.state.results}</p>
+            </div>
+    )
+  }
+}
+//{this.state.results.map((item, i) => <Issue key={i} item={item} />)}
+
+export default Requests;
