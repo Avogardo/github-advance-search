106aced17a6c88d1ff69f83e230640aeec31f876
Avogardo <avogardo0@gmail.com>
2016-11-20 00:02:03 +0100

    0.5

--- src/Requests.js
+++ src/Requests.js
@@ -7,21 +7,24 @@ class Requests extends Component {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
     this.state = {
+      query: '',
       results: []
     };
   }
 
+
+
   onSubmit (e) {
     e.preventDefault();
     const issue = e.target.value;
-
+console.log(issue);
     fetch("https://api.github.com/search/issues?q="+issue).then(function(response) {
         return response.json();
     }).then((data) => {
 
         let result = "";
         for(let i = 0; i < 30; i++) {
-            result += data.items[i].html_url + "<br>";
+            result += data.items[i].html_url + "  ";
         }
 
         this.setState({
@@ -38,7 +41,9 @@ class Requests extends Component {
     return (
             <div>
                 <h2>Szukaj</h2>
-                <input type="text" onChange={this.onSubmit} />
+
+                  <input type="text" onChange={this.onSubmit}/>
+
 
                 <h2>Wyniki:</h2>
                 <p>{this.state.results}</p>
